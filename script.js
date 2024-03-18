fd849719be8401ed1f53a1b90cab6527ab3a63f8
FranDAW1 <1922925@alu.murciaeduca.es>
2024-03-18 12:00:23 +0100

    Improve JS to allow more pages

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 $(() => {
   $('.buttons-header a').on('click', function () {
-    $('main, header').animate({
-      opacity: 0.75,
-      opacity: 0.5,
-      opacity: 0.25,
-      opacity: 0
-    }, 1100)
-    let viewportHeight = $(window).height();
-    let viewportWidth = $(window).width();
-    let h1Height = $('h1').outerHeight();
-    let h1Width = $('h1').outerWidth();
-    let topOffset = 115; // Change this value as needed
-    let leftOffset = 48; // Change this value as needed
-    let topPosition = ((viewportHeight / 2) - (h1Height / 2)) - topOffset;
-    let leftPosition = ((viewportWidth / 2) - (h1Width / 2)) - leftOffset;
-    $('h1').animate({
-      left: leftPosition + 'px',
-      top: topPosition + 'px',
-      fontSize: '3em',
-      fontSize: '5vh',
-      fontSize: '10vh'
-    }, 1100, function () {
-      // Animation complete.
-      window.location.href = '/';
-    })
-  })
-})
+    var url = $(this).data('url');
+    if(url == "/"){
+      $('main, header').animate({
+        opacity: 0
+      }, 1100);
+      let viewportHeight = $(window).height();
+      let viewportWidth = $(window).width();
+      let h1Height = $('h1').outerHeight();
+      let h1Width = $('h1').outerWidth();
+      let topOffset = 115; // Change this value as needed
+      let leftOffset = 48; // Change this value as needed
+      let topPosition = ((viewportHeight / 2) - (h1Height / 2)) - topOffset;
+      let leftPosition = ((viewportWidth / 2) - (h1Width / 2)) - leftOffset;
+      $('h1').animate({
+        left: leftPosition + 'px',
+        top: topPosition + 'px',
+        fontSize: '10vh'
+      }, 1100, function () {
+        // Animation complete.
+        window.location.href = url;
+      });
+    }else{
+      window.location.href = url;
+    }
+  });
+});
